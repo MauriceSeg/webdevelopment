5d177fe1728a623127d680bacf4be675532af73f
MauriceSeg <r1089381@student.vives.be>
2026-03-23 22:49:49 +0100

    sdf

--- Labo_18/Opdracht_nodes_1/scripts/index.js
+++ Labo_18/Opdracht_nodes_1/scripts/index.js
@@ -1,3 +1,2 @@
 let p = document.querySelectorAll("p");
-
 p[0].textContent = "Goed gedaan!";
--- Labo_18/Opdracht_nodes_2/scripts/index.js
+++ Labo_18/Opdracht_nodes_2/scripts/index.js
@@ -1,11 +1,7 @@
 let li = document.querySelectorAll("li");
-
 for(let i=0;i<li.length;i++){
     li[i].className="listitem";
 }
-
 let img = document.createElement("img");
-
 img.setAttribute("src","foto.jpg");
-
 document.body.appendChild(img);
